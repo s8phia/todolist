515dd892040bbd36d26797b48fa57d6b26f582a4
Sophia <sophiawg143@gmail.com>
2025-08-17 20:26:17 -0400

    .env added

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,5 @@
-export const PORT = 5555;
-export const mongoDbURL = 'mongodb+srv://dbUser:lol123@cluster0.xezni79.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+import dotenv from 'dotenv';
+dotenv.config(); 
+
+export const PORT = process.env.PORT || 5555;
+export const mongoDbURL = process.env.MONGODBURL;
